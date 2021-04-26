b0385a54ab74877311c382864e1381c21c03e15d
Colin Bondi <81669905+cmbondi@users.noreply.github.com>
2021-04-26 14:01:56 -0700

    Add showPage function
    Completed first version of showPage function

--- js/script.js
+++ js/script.js
@@ -3,14 +3,41 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
+// Function to create the LI element for each student
+function createLi(currentItem) {
+   // Create variables to hold new DOM elements
+   let li = document.createElement('li');
+   let div1 = document.createElement('div');
+   let img = document.createElement('img');
+   let h3 = document.createElement('h3');
+   let spanEmail = document.createElement('span');
+   let div2 = document.createElement('div');
+   let spanDate = document.createElement('span');
 
+   // Add values to each new DOM element
+   li.classList.add("student-item", "cf");
+   div1.classList.add("student-details");
+   img.classList.add("avatar");
+   img.src = `${currentItem.picture.large}`;
+   img.alt = "Profile Picture";
+   h3.innerHTML = `${currentItem.name.title} ${currentItem.name.first} ${currentItem.name.last}`;
+   spanEmail.classList.add("email");
+   spanEmail.innerHTML = `${currentItem.email}`;
+   div2.classList.add("joined-details");
+   spanDate.classList.add("date");
+   spanDate.innerHTML = `${currentItem.registered.date}`;
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
+   // Append new DOM elements to final Li item
+   div1.appendChild(img);
+   div1.appendChild(h3);
+   div1.appendChild(spanEmail);
+   li.appendChild(div1);
+   div2.appendChild(spanDate);
+   li.appendChild(div2);
+   
+   // Return the completed LI element
+   return li;
+}
 
 
 /*
@@ -18,7 +45,18 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
-
+function showPage(list, page) {
+   let startIndex = (page * 9) - 9;
+   let endIndex = page * 9;
+   let studentList = document.querySelector('.student-list');
+   studentList.innerHTML = '';
+   for (let i = 0; i<list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+         let studentItem = createLi(list[i]);
+         studentList.appendChild(studentItem);
+      }
+   }
+ }
 
 /*
 Create the `addPagination` function
@@ -28,3 +66,4 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
+showPage(data, 1);
