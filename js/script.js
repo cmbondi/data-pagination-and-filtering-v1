694fa68ab982860b5be0b03c92f32d99a2fe78ed
Colin Bondi <81669905+cmbondi@users.noreply.github.com>
2021-04-27 12:51:06 -0700

    cleanup of functions
    cleanup extra code from showPage function

--- js/script.js
+++ js/script.js
@@ -3,43 +3,6 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-// Function to create the LI element for each student
-function createLi(currentItem) {
-   // Create variables to hold new DOM elements
-   let li = document.createElement('li');
-   let div1 = document.createElement('div');
-   let img = document.createElement('img');
-   let h3 = document.createElement('h3');
-   let spanEmail = document.createElement('span');
-   let div2 = document.createElement('div');
-   let spanDate = document.createElement('span');
-
-   // Add values to each new DOM element
-   li.classList.add("student-item", "cf");
-   div1.classList.add("student-details");
-   img.classList.add("avatar");
-   img.src = `${currentItem.picture.large}`;
-   img.alt = "Profile Picture";
-   h3.innerHTML = `${currentItem.name.title} ${currentItem.name.first} ${currentItem.name.last}`;
-   spanEmail.classList.add("email");
-   spanEmail.innerHTML = `${currentItem.email}`;
-   div2.classList.add("joined-details");
-   spanDate.classList.add("date");
-   spanDate.innerHTML = `${currentItem.registered.date}`;
-
-   // Append new DOM elements to final Li item
-   div1.appendChild(img);
-   div1.appendChild(h3);
-   div1.appendChild(spanEmail);
-   li.appendChild(div1);
-   div2.appendChild(spanDate);
-   li.appendChild(div2);
-   
-   // Return the completed LI element
-   return li;
-}
-
-
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
@@ -52,8 +15,6 @@ function showPage(list, page) {
    studentList.innerHTML = '';
    for (let i = 0; i<list.length; i++) {
       if (i >= startIndex && i < endIndex) {
-         // let studentItem = createLi(list[i]);
-         // studentList.appendChild(studentItem);
          let studentItem = `
             <li class="student-item cf">
                <div class="student-details">
